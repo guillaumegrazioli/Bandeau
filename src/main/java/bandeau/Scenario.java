95f6174ce882370132895f65d6ffd97d22dedae4
Guillaume <guillaume.grazioli@etud.univ-jfc.fr>
2020-03-11 11:41:45 +0100

    classe effet + scenario

---
+++ src/main/java/bandeau/Scenario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author ggraziol
+ */
+public class Scenario {
+    
+    private HashMap<Effet,Integer> effets;
+    
+    
+}
