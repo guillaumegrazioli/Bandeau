1676265726b5ea4b088cfb610ba37842aa600e79
Claire <claire.jankowiak@etud.univ-jfc.fr>
2020-03-11 12:13:36 +0100

    Ajout effet Clignotant

---
+++ src/main/java/bandeau/Clignotant.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bandeau;
+
+/**
+ *
+ * @author cjankowi
+ */
+public class Clignotant extends Effet {
+    
+    private int nbclign;
+    
+    public Clignotant(Bandeau bandeau, int nbclign){
+        super(bandeau);
+        this.nbclign=nbclign;
+    }
+
+    @Override
+    public void run() {
+        String message = bandeau.getMessage();
+        for (int i = 0; i < this.nbclign; i++) {
+            bandeau.setMessage("");
+            bandeau.setMessage(message);
+        }
+    }
+    
+    
+    
+}
